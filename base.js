7ebf2337c230e400b1400f01bd4510c01b92bb3d
tuckeryazdani <84822334+tuckeryazdani@users.noreply.github.com>
2021-12-27 23:44:33 -0500

    Delete base.js

--- base.js
+++
@@ -1,6 +0,0 @@
-<ul>
-  <li><a class="active" href="https://tuckeryazdani.github.io/mywebsite/">Home</a></li>
-  <li><a href="about.html">About</a></li>
-  <li><a href="projects.html">Projects</a></li>
-  <li><a href="thesis.html">Thesis</a></li>
-</ul>
