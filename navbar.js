2fdb9ce276f3e08d7e447946e4b73d3f7fb15044
tuckeryazdani <84822334+tuckeryazdani@users.noreply.github.com>
2022-01-07 13:34:30 -0500

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,13 @@
+<link href="main.css" rel="stylesheet" id="nav-placeholder">
+<div class="topnav">
+  <a class="active" href="https://tuckeryazdani.github.io/MyWebsite/">Home</a>
+  <a href="about.html">About</a>
+  <a href="projects.html">Projects</a>
+  <a href="thesis.html">Thesis</a>
+</div>
+
+<script>
+$(function(){
+  $("#nav-placeholder").load("navbar.html");
+});
+</script>
