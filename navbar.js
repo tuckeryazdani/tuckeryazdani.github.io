7d362002ce483f294c4b823074efa3dd5d5d6dec
tuckeryazdani <84822334+tuckeryazdani@users.noreply.github.com>
2022-01-07 13:46:49 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,13 +1,11 @@
-<link href="main.css" rel="stylesheet" id="nav-placeholder">
-<div class="topnav">
+<script>
+document.write('
+  <link href="main.css" rel="stylesheet" id="nav-placeholder">
+  <div class="topnav">
   <a class="active" href="https://tuckeryazdani.github.io/MyWebsite/">Home</a>
   <a href="about.html">About</a>
   <a href="projects.html">Projects</a>
   <a href="thesis.html">Thesis</a>
-</div>
-
-<script>
-$(function(){
-  $("#nav-placeholder").load("navbar.html");
-});
+  </div>
+');
 </script>
