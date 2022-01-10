578e60d7d32aec4cc40fbf64614680042e4b96fe
tuckeryazdani <84822334+tuckeryazdani@users.noreply.github.com>
2022-01-10 12:43:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
   document.open();
   document.write("
-<link href="main.css" rel="stylesheet">
 <div class="topnav">
   <a class="active" href="https://tuckeryazdani.github.io/MyWebsite/">Home</a>
   <a href="about.html">About</a>
