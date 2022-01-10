e324aa1bdc16703569b96e43ed402482bd92d93d
tuckeryazdani <84822334+tuckeryazdani@users.noreply.github.com>
2022-01-10 13:09:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
   document.open();
-  document.write("<h1>TestisActive</h1>");
-  document.createElement("
-    <div class="topnav">
-    <a class="active" href="https://tuckeryazdani.github.io/MyWebsite/">Home</a>
-    <a href="about.html">About</a>
-    <a href="projects.html">Projects</a>
-    <a href="thesis.html">Thesis</a>
-    </div>");
+
+  document.write('<div class="topnav"> <a class="active" href="https://tuckeryazdani.github.io/MyWebsite/">Home</a><a href="about.html">About</a><a href="projects.html">Projects</a><a href="thesis.html">Thesis</a></div>"');
+                 
   document.close();
